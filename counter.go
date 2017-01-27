b256a097a5bb8a3f03f17e095b2f593c74574b41
yuhara <yuharacomplex@gmail.com>
2017-01-27 13:32:26 +0900

    CreateStateを移動

--- counter.go
+++ counter.go
@@ -10,20 +10,6 @@ type Counter struct {
 	c int64
 }
 
-type CounterCreator struct {
-}
-
-func (c *CounterCreator) CreateState(ctx *core.Context, params data.Map) (core.SharedState, error) {
-	cnt := &Counter{}
-	if v, ok := params["start"]; ok {
-		i, err := data.ToInt(v)
-		if err != nil {
-			return nil, err
-		}
-		cnt.c = i - 1
-	}
-	return cnt, nil
-}
 
 func (c *Counter) Terminate(ctx *core.Context) error {
 	return nil
--- plugin/plugin.go
+++ plugin/plugin.go
@@ -5,6 +5,21 @@ import (
 	"gopkg.in/sensorbee/sensorbee.v0/bql/udf"
 )
 
+type CounterCreator struct {
+}
+
+func (c *CounterCreator) CreateState(ctx *core.Context, params data.Map) (core.SharedState, error) {
+	cnt := &Counter{}
+	if v, ok := params["start"]; ok {
+		i, err := data.ToInt(v)
+		if err != nil {
+			return nil, err
+		}
+		cnt.c = i - 1
+	}
+	return cnt, nil
+}
+
 func init() {
 	udf.MustRegisterGlobalUDSCreator("my_counter", &CounterCreator{})
 }
