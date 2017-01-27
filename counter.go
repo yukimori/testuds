40d8328d277590def34aa75d2a7af98e85e30f60
yuhara <yuharacomplex@gmail.com>
2017-01-27 13:45:07 +0900

    修正

--- counter.go
+++ counter.go
@@ -2,12 +2,29 @@ package testuds
 
 import (
 	"sync/atomic"
+	"gopkg.in/sensorbee/sensorbee.v0/data"
 	"gopkg.in/sensorbee/sensorbee.v0/core"
 )
 
 type Counter struct {
 	c int64
 }
+
+type CounterCreator struct {
+}
+
+func (c *CounterCreator) CreateState(ctx *core.Context, params data.Map) (core.SharedState, error) {
+	cnt := &testuds.Counter{}
+	if v, ok := params["start"]; ok {
+		i, err := data.ToInt(v)
+		if err != nil {
+			return nil, err
+		}
+		cnt.c = i - 1
+	}
+	return cnt, nil
+}
+
 
 
 func (c *Counter) Terminate(ctx *core.Context) error {
--- plugin/plugin.go
+++ plugin/plugin.go
@@ -2,25 +2,10 @@ package plugin
 
 import (
 	"github.com/yukimori/testuds"
-	"gopkg.in/sensorbee/sensorbee.v0/data"
 	"gopkg.in/sensorbee/sensorbee.v0/bql/udf"
 )
 
-type CounterCreator struct {
-}
-
-func (c *CounterCreator) CreateState(ctx *core.Context, params data.Map) (core.SharedState, error) {
-	cnt := &Counter{}
-	if v, ok := params["start"]; ok {
-		i, err := data.ToInt(v)
-		if err != nil {
-			return nil, err
-		}
-		cnt.c = i - 1
-	}
-	return cnt, nil
-}
 
 func init() {
-	udf.MustRegisterGlobalUDSCreator("my_counter", &CounterCreator{})
+	udf.MustRegisterGlobalUDSCreator("my_counter", &testuds.CounterCreator{})
 }
